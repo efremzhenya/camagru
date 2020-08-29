70b188468debc89f7b7ef7aef99e15edb3e4eaac
Louise Seema <lseema@mi-n3.msk.21-school.ru>
2020-08-29 05:45:29 +0300

    init php files fix

--- www/app/bootstrap.php
+++ www/app/bootstrap.php
@@ -2,6 +2,8 @@
 require_once 'core/model.php';
 require_once 'core/view.php';
 require_once 'core/controller.php';
+require_once 'core/dao.php';
 require_once 'core/route.php';
-Route::start(); // запускаем маршрутизатор
+
+Route::start();
 ?>
